9016131093a45b1e8e74d8fe3452d518170676f7
Jayme Dev <jayme35371@gmail.com>
2025-06-15 19:48:03 -0500

    new esphere colors to PC

--- src/sections/3-skills/skills_Page.jsx
+++ src/sections/3-skills/skills_Page.jsx
@@ -1,15 +1,15 @@
 /*tagcloud sirve para hacer una esfera 3d rotando*/
 import TagCloud from "TagCloud";
 import { useEffect } from "react"
-
+import "./assets/app.scss";
 
 
 export default function Skills() {
   useEffect(() => {
     const container = ".container";
-    const text = ["", "", "", "", "", "", "", "", "", "","","",""];
+    const text = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
 
-    const options = {
+    let options = {
       radius: 200,
       maxSpeed: "slow",
       initSpeed: "slow",
@@ -18,10 +18,37 @@ export default function Skills() {
       itemClass: "items",
     };
 
+    if (window.matchMedia("(min-width: 768px) and (max-width: 1279px)").matches) {
+      options = {
+        radius: 350,
+        maxSpeed: "slow",
+        initSpeed: "slow",
+        keep: true,
+        containerClass: "circulo",
+        itemClass: "items",
+      };
+
+    } else if (window.matchMedia("(min-width: 1280px)").matches) {
+      options = {
+        radius: 420,
+        maxSpeed: "slow",
+        initSpeed: "slow",
+        keep: true,
+        containerClass: "circulo",
+        itemClass: "items",
+      };
+
+    }
+
+
     TagCloud(container, text, options);
 
     const span1 = document.querySelector(".circulo .items:nth-child(1)");
     span1.style.backgroundImage = "";
+
+
+
+
   }, []);
   return (
     <div className="skills" id="skills">
